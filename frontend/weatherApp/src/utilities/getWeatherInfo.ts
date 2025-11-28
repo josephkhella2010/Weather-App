3c355cbcea4933661f4b0deffb4d3f186013bc15
Joseph Khella <josephkhella2010@gmail.com>
2025-11-28 21:19:58 +0100

    change  api link

--- frontend/weatherApp/src/utilities/getWeatherInfo.ts
+++ frontend/weatherApp/src/utilities/getWeatherInfo.ts
@@ -8,7 +8,7 @@ export const getWeatherInfo = async (cityName: string) => {
   }
   try {
     const response = await axios.get(
-      `http://localhost:3700/api/get/get-weather?city=${cityName}`
+      ` https://weather-app-backend-1j65.onrender.com/get-weather?city=${cityName}`
     );
 
     return response.data;
