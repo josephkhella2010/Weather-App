11e6edf4a39f17b4038176d4c68459e6cbcc4463
Joseph Khella <josephkhella2010@gmail.com>
2025-12-05 21:30:16 +0100

    fix 5 day link

--- frontend/weatherApp/src/utilities/getWeatherInfoDays.ts
+++ frontend/weatherApp/src/utilities/getWeatherInfoDays.ts
@@ -6,8 +6,11 @@ export const getWeatherInfoDays = async (cityName: string) => {
       toast.error("Please enter A city name");
       return;
     }
-    const response = await axios.get(
+    /*   const response = await axios.get(
       `http://localhost:3700/api/get-weather-days?city=${cityName}`
+    ); */
+    const response = await axios.get(
+      `https://weather-app-backend-1j65.onrender.com/api/get-weather-days?city=${cityName}`
     );
 
     return response.data;
