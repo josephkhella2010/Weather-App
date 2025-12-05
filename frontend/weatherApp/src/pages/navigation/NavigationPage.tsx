240ff4380d6ea1e7f1ef5e54ac7279e379e74c7f
Joseph Khella <josephkhella2010@gmail.com>
2025-12-05 20:26:24 +0100

    adjust navigation

--- frontend/weatherApp/src/pages/navigation/NavigationPage.tsx
+++ frontend/weatherApp/src/pages/navigation/NavigationPage.tsx
@@ -1,16 +1,34 @@
 import { createUseStyles } from "react-jss";
 import DesktopNavBar from "./childComponent/DesktopNavBar";
 import MobileNavBar from "./childComponent/MobileNavBar";
+import { useEffect, useState } from "react";
 
-const useStyles = createUseStyles({});
+const useStyles = createUseStyles({
+  navbarWrapper: {
+    width: "100%",
+  },
+});
 export default function NavigationPage() {
   const classes = useStyles();
+  const [isWidth, setIsWidth] = useState<boolean>(false);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 750) {
+        setIsWidth(true);
+      } else {
+        setIsWidth(false);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
 
   return (
-    <div>
-      {/*       <DesktopNavBar />
-       */}{" "}
-      <MobileNavBar />
+    <div className={classes.navbarWrapper}>
+      {isWidth ? <MobileNavBar /> : <DesktopNavBar />}
     </div>
   );
 }
