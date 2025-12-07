3aeb592a7c4fb1a6b2507c1a2f8b859ad00664a8
Joseph Khella <josephkhella2010@gmail.com>
2025-12-07 17:09:51 +0100

    fix slice

--- frontend/weatherApp/src/pages/Home/childComponent/WeatherDetails.tsx
+++ frontend/weatherApp/src/pages/Home/childComponent/WeatherDetails.tsx
@@ -1,4 +1,4 @@
-import { createUseStyles } from "react-jss";
+/* import { createUseStyles } from "react-jss";
 import SecondContainer from "./SecondContainer";
 import WeatherInfoContainer from "./WeatherInfoContainer";
 
@@ -38,12 +38,69 @@ export default function WeatherDetails({
           transition: "transform 0.5s ease-in-out",
         }}
       >
-        {/* First Section*/}
         <div className={classes.WeatherContainerMainSection}>
           <SecondContainer />
         </div>
 
-        {/* Second Section */}
+        <div className={classes.WeatherContainerMainSection}>
+          <WeatherInfoContainer />
+        </div>
+      </div>
+    </div>
+  );
+}
+ */
+import { createUseStyles } from "react-jss";
+import SecondContainer from "./SecondContainer";
+import WeatherInfoContainer from "./WeatherInfoContainer";
+
+const useStyles = createUseStyles({
+  WeatherMainContainer: {
+    overflowX: "hidden",
+    width: "100%",
+  },
+  WeatherContainer: {
+    display: "flex",
+    width: "200%",
+  },
+  WeatherContainerMainSection: {
+    width: "50%",
+    flexShrink: 0,
+  },
+});
+
+interface WeatherDetailsProps {
+  isCityCorrect: boolean;
+  storedCityCorrect: boolean;
+}
+
+export default function WeatherDetails({
+  isCityCorrect,
+  storedCityCorrect,
+}: WeatherDetailsProps) {
+  const classes = useStyles();
+
+  const storedCity = sessionStorage.getItem("cityName");
+  const hasStoredCity = Boolean(storedCity);
+
+  // MAIN SWITCHING LOGIC
+  const showSecond = !hasStoredCity || !isCityCorrect || !storedCityCorrect;
+
+  return (
+    <div className={classes.WeatherMainContainer}>
+      <div
+        className={classes.WeatherContainer}
+        style={{
+          transform: showSecond ? "translateX(0%)" : "translateX(-50%)",
+          transition: "transform 0.7s ease",
+        }}
+      >
+        {/* empty section*/}
+        <div className={classes.WeatherContainerMainSection}>
+          <SecondContainer />
+        </div>
+
+        {/* info section*/}
         <div className={classes.WeatherContainerMainSection}>
           <WeatherInfoContainer />
         </div>
